7efcfb5c242761a37227d1cbb571cc581f6edceb
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-07 23:00:36 +0200

    replaced ffmpeg-full with simple ffmpeg

--- install/02-audio-and-video-codecs.sh
+++ install/02-audio-and-video-codecs.sh
@@ -3,7 +3,7 @@ yay -S --noconfirm --needed \
   wireplumber pipewire-pulse pipewire-alsa pipewire-audio \
   pipewire-zeroconf pwvucontrol pamixer \
   gst-plugins-base gst-plugins-good gst-plugin-ugly gst-plugins-bad gst-plugin-va \
-  gst-plugin-pipewire ffmpeg-full flac alac-git wavpack \
+  gst-plugin-pipewire ffmpeg flac alac-git wavpack \
   faac libwebp libavif libheif dav1d \
   x265 xvidcore x264 libvpx libmpeg2 \
   libva-utils vdpauinfo vulkan-tools
