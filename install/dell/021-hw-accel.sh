7ad6a0f7036bf21e2e142b8d1a9781c42341dfe7
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-08 00:23:36 +0200

    removed libva-intel-driver-hybrid

--- install/dell/021-hw-accel.sh
+++ install/dell/021-hw-accel.sh
@@ -1,4 +1,4 @@
 # Install dell specific hw-acceleration packages
 yay -S --noconfirm -needed \
-  mesa libva-mesa-driver intel-media-driver libva-intel-driver-hybrid intel-hybrid-codec-driver-git \
+  mesa libva-mesa-driver intel-media-driver intel-hybrid-codec-driver-git \
   vulkan-intel libvpl vpl-gpu-rt intel-media-sdk
