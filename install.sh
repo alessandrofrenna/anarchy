b772fff0053ed9fc888bdfe83ca6258e3e6970e8
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-08 01:26:30 +0200

    fix error on ORPHANCOUNT

--- install.sh
+++ install.sh
@@ -43,7 +43,7 @@ echo -e "\nUpdating database"
 sudo updatedb
 
 # Remove orphaned packages and clean cache
-ORPHANCOUNT=pacman -Qdtq | wc -l
+ORPHANCOUNT=$(pacman -Qtdq) | wq -l
 if [ $ORPHANCOUNT -gt 0 ] then
   echo -e "\nRemoving orphan"
   sudo pacman -Rns $(pacman -Qdtq)
