bcd38ba5fade60ecb90f6307a43f27e3c83f19e2
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-08 00:29:30 +0200

    restored 021-hw-accell.sh, fix on --needed

--- install/dell/021-hw-accel.sh
+++ install/dell/021-hw-accel.sh
@@ -1,4 +1,4 @@
 # Install dell specific hw-acceleration packages
-yay -S --noconfirm -needed \
-  mesa libva-mesa-driver intel-media-driver vulkan-intel libvpl \
-  vpl-gpu-rt intel-media-sdk
+yay -S --noconfirm --needed \
+  mesa libva-mesa-driver intel-media-driver libva-intel-driver-hybrid intel-hybrid-codec-driver-git \
+  vulkan-intel libvpl vpl-gpu-rt intel-media-sdk
