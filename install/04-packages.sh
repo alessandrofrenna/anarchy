d3d177a9fb771b6d5b519af835290361de16874c
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-09 00:18:27 +0200

    replaced spotify with spotify-launcher

--- install/04-packages.sh
+++ install/04-packages.sh
@@ -2,6 +2,6 @@
 yay -S --noconfirm --needed \
   brightnessctl wl-clipboard wl-clip-persist clipse-bin playerctl \
   fcitx5 fcitx5-gtk fcitx5-qt fcitx5-configtool ffmpegthumbnailer \
-  chromium mpv imv bitwarden-bin spotify \
+  chromium mpv imv bitwarden-bin spotify-launcher \
   zen-browser-bin thunar thunar-archive-plugin thunar-vcs-plugin thunar-volman \
   qalculate-gtk tumbler
