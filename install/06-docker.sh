5a6778c6a93ba376afd696c4ff1e3dbe6125beec
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-10 01:53:16 +0200

    changed 06-docker.sh script

--- install/06-docker.sh
+++ install/06-docker.sh
@@ -1,12 +1,22 @@
 # Install docker, docker-compose and lazydocker
 yay -S --noconfirm --needed docker docker-compose lazydocker-bin
 
-# Limit the log size to avoid running out of disk
-sudo mkdir -p /etc/docker
-echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+echo "Ensure Docker config is set"
+if [[ ! -f /etc/docker/daemon.json ]]; then
+  # Limit the log size to avoid running out of disk
+  sudo mkdir -p /etc/docker
+  echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+fi
 
 # Enable the docker service
-sudo systemctl enable --now docker.service
+docker_status="$(systemctl is-active docker.service)"
+if [ "${docker_status}" = "inactive" ]; then
+  echo -e "\nEnabling docker service..."
+  sudo systemctl enable --now docker.service
+else
+  echo -e "\nDocker already enabled"
+fi
+
 
 # Add the current user to the docker group
 sudo usermod -aG docker ${USER}
