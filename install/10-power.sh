182abc72542e7144884aff3f9182b2559706d772
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-07-09 02:33:26 +0200

    changed ppd plugin settings

--- install/10-power.sh
+++ install/10-power.sh
@@ -1,5 +1,7 @@
 yay -S --noconfirm powertop power-profiles-daemon
 
+sudo systemctl enable --now power-profiles-daemon.service
+
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # This computer runs on a battery
   powerprofilesctl set power-saver || true
