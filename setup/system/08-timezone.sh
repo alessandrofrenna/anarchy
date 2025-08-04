9d7bf70b6d0cf2b0728678e4b35cc863a65e6952
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-08-04 21:51:04 +0200

    add tzupdate features

---
+++ setup/system/08-timezone.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -euo pipefail
+
+# Original reference https://github.com/basecamp/omarchy/blob/dev/install/config/timezones.sh
+# ======================================================================================
+# Based on Omarchy script: timezone.sh
+# ======================================================================================
+readonly FILE_PATH="/etc/sudoers.d/anarchy-tzupdate"
+if ! command -v tzupdate &>/dev/null; then
+  yay -S --noconfirm --needed tzupdate
+  sudo tee "${FILE_PATH}" >/dev/null <<EOF
+%wheel ALL=(root) NOPASSWD: /usr/bin/tzupdate, /usr/bin/timedatectl
+EOF
+  sudo chmod 0440 "${FILE_PATH}"
+fi
