ca2d4bdd9eb6eb4e0a23e71bbcaeb939d654d22b
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-08-19 14:33:34 +0200

    install flatpak + enable auto-update timer

---
+++ setup/necessary/02-flatpak.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail 
+ 
+echo -e "⏳ Installing flatpak..."
+sudo pacman -S -noconfirm --needed flatpak
+echo -e "✅ flatpak installed"
+
+# Enable timer to update installed flatpaks automatically
+if ! systemctl is-enabled --user --quiet update-user-flatpaks.timer ; then
+  echo -e "🔧 Enabling update-user-flatpaks timer..."
+  systemctl --user enable --now update-user-flatpaks.timer
+  echo -e "✅ update-user-flatpaks timer enabled"
+fi
