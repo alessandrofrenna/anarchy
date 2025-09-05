ac5cefb39aed3f28d8060464c1e8aa7368e6197b
Alessandro Frenna <alessandrofrenna95@gmail.com>
2025-09-06 00:56:41 +0200

    change {intel|amd} pstate driver mode

--- setup/system/07-power.sh
+++ setup/system/07-power.sh
@@ -24,11 +24,29 @@ optimize_nvme() {
   echo "✅ set nvme.noacpi=1 successfully"
 }
 
+change_pstate_mode() {
+  local pstate_driver_mode
+  # Find the architecture microcode to install
+  cpu_vendor=$(awk -F ': ' '/vendor_id/ {print $2}' /proc/cpuinfo | uniq)
+  if [ "$cpu_vendor" = "GenuineIntel" ]; then
+    echo -e "💻 Found GenuineIntel CPU, setting intel_pstate=passive"
+    pstate_driver_mode = "intel_pstate=passive"
+  elif [ "$cpu_vendor" = "AuthenticAMD" ]; then
+    echo -e "💻 Found AuthenticAMD CPU, setting amd-pstate=guided"
+    pstate_driver_mode = "amd-pstate=guided"
+  fi
+
+  local CMDLINE_DIR="/etc/cmdline.d"
+  local PASSIVE_PSTATE_CONF_FILE="${CMDLINE_DIR}/passive_pstate.conf"
+
+  echo "${pstate_driver_mode}" | sudo tee "${PASSIVE_PSTATE_CONF_FILE}" >/dev/null
+}
+
 if ls /sys/class/power_supply/BAT* &>/dev/null; then
   # Laptop with battery - install monitoring tools
   echo -e "🔋 Laptop detected"
   echo -e "⏳ Installing power monitoring tools..."
-  sudo pacman -S --noconfirm --needed powertop acpi tlp
+  sudo pacman -S --noconfirm --needed powertop acpi tlp cpupower
   echo "✅ Power monitoring tools installed"
 
   # Enable battery monitoring timer for low battery notifications
@@ -41,6 +59,8 @@ if ls /sys/class/power_supply/BAT* &>/dev/null; then
   if lsblk -no PKNAME $(findmnt -n -o SOURCE /) | grep -q "nvme"; then
     optimize_nvme
   fi
+
+  change_pstate_mode
 
   # Enable tlp
   if ! sudo systemctl is-enabled --quiet tlp.service ; then
